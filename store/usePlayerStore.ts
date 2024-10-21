d43b25d482b964b3cb51060c218dd89201208a3d
Orkun Ozekin <orkunozekin@gmail.com>
2024-10-20 19:54:36 -0500

    Add LS persistance to zustand stores

--- store/usePlayerStore.ts
+++ store/usePlayerStore.ts
@@ -1,32 +1,45 @@
 import { create } from 'zustand'
 import { v4 as uuidv4 } from 'uuid'
 import { PlayersState } from '@/interfaces/Player.interface'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
-// Create the Zustand store
-export const usePlayersStore = create<PlayersState>(set => ({
-  players: [],
-  addPlayers: names =>
-    set(state => ({
-      players: [
-        ...state.players,
-        ...names.map(name => ({
-          id: uuidv4(),
-          name,
+//store for all players
+export const usePlayersStore = create<PlayersState>()(
+  persist(
+    set => ({
+      players: [],
+
+      addPlayers: names =>
+        set(state => ({
+          players: [
+            ...state.players,
+            ...names.map(name => ({
+              id: uuidv4(),
+              name,
+            })),
+          ],
         })),
-      ],
-    })),
-  deletePlayer: id =>
-    set(state => ({
-      players: state.players.filter(player => player.id !== id),
-    })),
-  editPlayer: (id, name) =>
-    set(state => ({
-      players: state.players.map(player => {
-        if (player.id === id) {
-          player.name = name
-        }
-        return player
-      }),
-    })),
-  clearPlayers: () => set({ players: [] }),
-}))
+
+      deletePlayer: id =>
+        set(state => ({
+          players: state.players.filter(player => player.id !== id),
+        })),
+
+      editPlayer: (id, name) =>
+        set(state => ({
+          players: state.players.map(player => {
+            if (player.id === id) {
+              player.name = name
+            }
+            return player
+          }),
+        })),
+
+      clearPlayers: () => set({ players: [] }),
+    }),
+    {
+      name: 'players-storage', // localStorage key
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+)
--- store/useTeamsStore.ts
+++ store/useTeamsStore.ts
@@ -2,6 +2,7 @@ import { Player } from '@/interfaces/Player.interface'
 import { Team, TeamColor } from '@/interfaces/Team.interface'
 import { capitalizeFirstLetter } from '@/lib/stringUtils'
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 interface TeamsState {
   teams: Team[]
@@ -11,94 +12,107 @@ interface TeamsState {
   clearTeams: () => void
 }
 
-const useTeamsStore = create<TeamsState>(set => ({
-  teams: [],
+const useTeamsStore = create<TeamsState>()(
+  persist(
+    set => ({
+      teams: [],
 
-  generateTeams: (players, teamCount = 2) => {
-    if (!players.length) return
-    // Shuffle players using Fisher-Yates algorithm
-    const shuffledPlayers = [...players]
-    for (let i = shuffledPlayers.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      ;[shuffledPlayers[i], shuffledPlayers[j]] = [
-        shuffledPlayers[j],
-        shuffledPlayers[i],
-      ]
-    }
+      generateTeams: (players, teamCount = 2) => {
+        if (!players.length) return
 
-    // Determine the minimum number of teams
-    let numberOfTeams = teamCount
-    if (!teamCount) numberOfTeams = 2
-    else if (shuffledPlayers.length > 11 * teamCount) {
-      numberOfTeams = Math.ceil(shuffledPlayers.length / 11)
-    }
-
-    // Ensure at least two teams are created
-    numberOfTeams = Math.max(2, numberOfTeams)
-
-    // Define possible colors for teams
-    const colors = ['ORANGE', 'GREEN', 'BLUE', 'SHIRTS'] // Orange, Green, Light Blue
-
-    // Generate teams with random colors
-    const nameCounts: Record<string, number> = {}
-    const teams: Team[] = Array.from({ length: numberOfTeams }, (_, index) => {
-      /**team name will be the color --
-       * it will be capitalized and have a number appended if there are multiple teams with the same color
-       */
-      const baseName = colors[index % colors.length]
-      nameCounts[baseName] = (nameCounts[baseName] || 0) + 1
-      const name =
-        nameCounts[baseName] > 1
-          ? `${baseName} ${nameCounts[baseName]}`
-          : baseName
-      return {
-        id: `team-${index + 1}`,
-        name: `${capitalizeFirstLetter(name.toLowerCase()) as TeamColor}`,
-        players: [],
-        color: colors[index % colors.length] as TeamColor,
-      }
-    })
-
-    // Distribute players evenly among teams
-    shuffledPlayers.forEach((player, index) => {
-      teams[index % numberOfTeams].players.push(player)
-    })
-
-    set({ teams })
-  },
-
-  removeTeam: teamId =>
-    set(state => ({
-      teams: state.teams.filter(team => team.id !== teamId),
-    })),
-
-  reassignPlayer: (playerId, targetTeamId) =>
-    set(state => {
-      // Remove player from their current team
-      let playerToReassign: Player | null = null
-      const updatedTeams = state.teams.map(team => {
-        const filteredPlayers = team.players.filter(player => {
-          if (player.id === playerId) {
-            playerToReassign = player
-            return false
-          }
-          return true
-        })
-        return { ...team, players: filteredPlayers }
-      })
-
-      // Add player to the target team
-      if (playerToReassign) {
-        const targetTeamIndex = updatedTeams.findIndex(
-          team => team.id === targetTeamId
-        )
-        if (targetTeamIndex !== -1) {
-          updatedTeams[targetTeamIndex].players.push(playerToReassign)
+        // Shuffle players using Fisher-Yates algorithm
+        const shuffledPlayers = [...players]
+        for (let i = shuffledPlayers.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1))
+          ;[shuffledPlayers[i], shuffledPlayers[j]] = [
+            shuffledPlayers[j],
+            shuffledPlayers[i],
+          ]
         }
-      }
-      return { teams: updatedTeams }
+
+        // Determine the minimum number of teams
+        let numberOfTeams = teamCount
+        if (!teamCount) numberOfTeams = 2
+        else if (shuffledPlayers.length > 11 * teamCount) {
+          numberOfTeams = Math.ceil(shuffledPlayers.length / 11)
+        }
+
+        // Ensure at least two teams are created
+        numberOfTeams = Math.max(2, numberOfTeams)
+
+        // Define possible colors for teams
+        const colors = ['ORANGE', 'GREEN', 'BLUE', 'SHIRTS'] // Orange, Green, Light Blue
+
+        // Generate teams with random colors
+        const nameCounts: Record<string, number> = {}
+        const teams: Team[] = Array.from(
+          { length: numberOfTeams },
+          (_, index) => {
+            /**team name will be the color --
+             * it will be capitalized and have a number appended if there are multiple teams with the same color
+             */
+            const baseName = colors[index % colors.length]
+            nameCounts[baseName] = (nameCounts[baseName] || 0) + 1
+            const name =
+              nameCounts[baseName] > 1
+                ? `${baseName} ${nameCounts[baseName]}`
+                : baseName
+            return {
+              id: `team-${index + 1}`,
+              name: `${capitalizeFirstLetter(name.toLowerCase()) as TeamColor}`,
+              players: [],
+              color: colors[index % colors.length] as TeamColor,
+            }
+          }
+        )
+
+        // Distribute players evenly among teams
+        shuffledPlayers.forEach((player, index) => {
+          teams[index % numberOfTeams].players.push(player)
+        })
+
+        set({ teams })
+      },
+
+      removeTeam: teamId =>
+        set(state => ({
+          teams: state.teams.filter(team => team.id !== teamId),
+        })),
+
+      reassignPlayer: (playerId, targetTeamId) =>
+        set(state => {
+          // Remove player from their current team
+          let playerToReassign: Player | null = null
+          const updatedTeams = state.teams.map(team => {
+            const filteredPlayers = team.players.filter(player => {
+              if (player.id === playerId) {
+                playerToReassign = player
+                return false
+              }
+              return true
+            })
+            return { ...team, players: filteredPlayers }
+          })
+
+          // Add player to the target team
+          if (playerToReassign) {
+            const targetTeamIndex = updatedTeams.findIndex(
+              team => team.id === targetTeamId
+            )
+            if (targetTeamIndex !== -1) {
+              updatedTeams[targetTeamIndex].players.push(playerToReassign)
+            }
+          }
+          return { teams: updatedTeams }
+        }),
+
+      clearTeams: () => set({ teams: [] }),
     }),
-  clearTeams: () => set({ teams: [] }),
-}))
+    {
+      name: 'teams-storage', // localStorage key
+      storage: createJSONStorage(() => localStorage), // use JSON storage with localStorage
+    }
+  )
+)
 
 export default useTeamsStore
