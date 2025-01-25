048f2c644814d46d5a1b0847b091f29ad830fc64
Orkun Ozekin <orkunozekin@gmail.com>
2025-01-25 08:52:44 -0600

    Add ability to remove player from a team

--- components/teams/TeamPlayerRow.tsx
+++ components/teams/TeamPlayerRow.tsx
@@ -9,13 +9,14 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { useTeamsStore } from '@/store/useTeamsStore'
+import TrashIcon from '../icons/TrashIcon'
 
 type Props = {
   team: Team
 }
 
 export default function TeamPlayerRow({ team }: Props) {
-  const { teams, reassignPlayer } = useTeamsStore()
+  const { teams, reassignPlayer, removeTeamPlayer } = useTeamsStore()
   const onReassignPlayer = (playerId: string, targetTeamId: any) => {
     reassignPlayer(playerId, targetTeamId)
   }
@@ -29,22 +30,27 @@ export default function TeamPlayerRow({ team }: Props) {
           className="flex list-inside list-decimal items-center justify-between py-1 font-semibold text-black"
           key={player.id}
         >
-          <p>{player.name}</p>
-          <Select onValueChange={value => onReassignPlayer(player.id, value)}>
-            <SelectTrigger className="w-[180px] border-neutral-10 font-normal">
-              <SelectValue placeholder="Transfer to" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Teams</SelectLabel>
-                {teamReassignOptions.map(team => (
-                  <SelectItem key={team.id} value={team.id}>
-                    {team.name}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          <p className="w-[50%]">{player.name}</p>
+          <section className="flex w-[50%] items-center gap-1">
+            <Select onValueChange={value => onReassignPlayer(player.id, value)}>
+              <SelectTrigger className="border-neutral-10 font-normal">
+                <SelectValue placeholder="Transfer to" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Teams</SelectLabel>
+                  {teamReassignOptions.map(team => (
+                    <SelectItem key={team.id} value={team.id}>
+                      {team.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <div className="rounded-md border border-neutral-10 p-2">
+              <TrashIcon onClick={() => removeTeamPlayer(player.id)} />
+            </div>
+          </section>
         </li>
       ))}
     </section>
